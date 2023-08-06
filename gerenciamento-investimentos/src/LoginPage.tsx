9af89939e8c566e50266c6de0cd5e12b52b2c78f
Lucas Ayrosa <40068293+LucasAyrosa@users.noreply.github.com>
2023-08-06 14:13:08 -0300

    feat: comunicação com API

--- gerenciamento-investimentos/src/LoginPage.tsx
+++ gerenciamento-investimentos/src/LoginPage.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Typography, TextField, Button, Link } from '@mui/material';
+import { useNavigate } from 'react-router';
+import { login } from './services/api';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginPage: React.FC = () => {
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const loginData = {email, password};
+            await login(loginData);
+            navigate('/dashboard')
+        } catch (error) {
+            console.error('Error login:', error);
+            toast.error('Erro no login', {
+                position: 'top-right',
+                autoClose: 3000,
+              });
+        }
+    }
+
   return (
     <Container maxWidth="sm">
       <Typography variant="h4" align="center" gutterBottom marginTop={5}>
         Login
       </Typography>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextField
           label="E-mail"
           fullWidth
           margin="normal"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
         <TextField
           label="Senha"
           fullWidth
           margin="normal"
           type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
-        <Button variant="contained" color="primary" fullWidth>
+        <Button variant="contained" color="primary" fullWidth type='submit'>
           Entrar
         </Button>
       </form>
