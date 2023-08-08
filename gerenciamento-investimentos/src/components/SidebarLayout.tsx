6af87f56bb4402ee90600b989f7c0e64a8867745
Lucas Ayrosa <40068293+LucasAyrosa@users.noreply.github.com>
2023-08-07 21:18:48 -0300

    feat: cabeçalho e menu lateral

--- gerenciamento-investimentos/src/components/SidebarLayout.tsx
+++ gerenciamento-investimentos/src/components/SidebarLayout.tsx
@@ -1,38 +1,97 @@
-import React from 'react';
-import { Link, Navigate, Outlet } from 'react-router-dom';
-import { CssBaseline, Drawer, List, ListItem, ListItemText, AppBar, Toolbar, Typography, Container } from '@mui/material';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import { useAuth } from '../contexts/AuthContext';
+import AppBar from '@mui/material/AppBar';
+import { Container } from '@mui/material';
+import { Navigate, Outlet } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 
 const SidebarLayout: React.FC = () =>  {
-const {isLoggedIn} = useAuth();
+  const {isLoggedIn} = useAuth();
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return isLoggedIn ? 
-    (< >
-      <AppBar position="fixed">
+    (
+      <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" >
         <Toolbar>
-          <Typography variant="h6" noWrap>
-            Sidebar Layout
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            GIFx - Gerenciamento de Investimentos Financeiros
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
-        variant="permanent"
+        sx={{
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
       >
-        <div  />
+        {/* <DrawerHdeader> */}
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        {/* </DrawerHdeader> */}
+        <Divider />
         <List>
-          <ListItem  component={Link} to="/dashboard">
-            <ListItemText primary="Dashboard" />
-          </ListItem>
+          {['Dashboard', 'Operações', 'Proventos'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
         </List>
       </Drawer>
-      <main >
-        <div />
-        <Container>
+      {/* <Main open={open}>
+        <DrawerHeader /> */}
+        <Container sx={{marginTop: '64px'}}>
           <Outlet /> 
         </Container>
-      </main>
-    </>)
+      {/* </Main> */}
+    </Box>)
     :<Navigate to="/login" />;
 };
 
